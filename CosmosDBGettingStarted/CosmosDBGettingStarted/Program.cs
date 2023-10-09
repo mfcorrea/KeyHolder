d9cc17c23407e3440295fbe17259c91314f60cfb
Marcelo Corrêa <marcelofcorrea@gmail.com>
2023-10-09 18:14:50 -0300

    Add project files.

---
+++ CosmosDBGettingStarted/CosmosDBGettingStarted/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Cosmos;
+
+
+namespace CosmosDBGettingStarted
+{
+    public class Program
+    {
+        // ADD THIS PART TO YOUR CODE
+
+        // The Azure Cosmos DB endpoint for running this sample.
+        private static readonly string EndpointUri = "<your endpoint here>";
+        // The primary key for the Azure Cosmos account.
+        private static readonly string PrimaryKey = "<your primary key>";
+
+        // The Cosmos client instance
+        private CosmosClient cosmosClient;
+
+        // The database we will create
+        private Database database;
+
+        // The container we will create.
+        private Container container;
+
+        // The name of the database and container we will create
+        private string databaseId = "FamilyDatabase";
+        private string containerId = "FamilyContainer";
+        public async Task GetStartedDemoAsync()
+        {
+            // Create a new instance of the Cosmos Client
+            this.cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+        }
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Beginning operations...\n");
+                Program p = new Program();
+                await p.GetStartedDemoAsync();
+
+            }
+            catch (CosmosException cosmosException)
+            {
+                Console.WriteLine("Cosmos Exception with Status {0} : {1}\n", cosmosException.StatusCode, cosmosException);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e);
+            }
+            finally
+            {
+                Console.WriteLine("End of demo, press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+
+}
---
+++ claviculario/claviculario/Form1.cs
@@ -0,0 +1,10 @@
+namespace claviculario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
